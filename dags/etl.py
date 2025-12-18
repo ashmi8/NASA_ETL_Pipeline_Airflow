c40d57bb5e36150434c8ec49b48393859e69cb3c
ashmi8 <ashmijha8@gmail.com>
2025-12-18 14:30:14 +0100

    Updated files:   dags/etl.py   - Added pendulum for date handling and response_filter to HttpOperator.   - Added Step 6: Define task dependencies.   requirements.txt   - Added apache-airflow-providers-http,     apache-airflow-providers-postgres to requirements.

--- dags/etl.py
+++ dags/etl.py
@@ -1,15 +1,15 @@
 from airflow import DAG
-from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.http.operators.http import HttpOperator
 from airflow.decorators import task
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.utils.dates import days_ago
+import pendulum
 import json
 
 
 ## Define the DAG
 with DAG(
     dag_id= 'nasa_apod_etl',
-    start_date= days_ago(1),
+    start_date= pendulum.now('UTC').subtract(days=1),
     schedule= '@daily',
     catchup= False,
 
@@ -28,7 +28,7 @@ with DAG(
             title VARCHAR(255),
             explanation TEXT,
             url TEXT,
-            date DATE
+            date DATE,
             media_type VARCHAR(50)
         );"""
 
@@ -37,13 +37,14 @@ with DAG(
 
     ## Step 2: Extract the NASA API Data(APOD)-Astronomy Picture of the Day[Extract pipeline]
     ## Sample API url: https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
-    extract_apod_data = SimpleHttpOperator(
+    extract_apod_data = HttpOperator(
         task_id='extract_apod_data',
         http_conn_id='nasa_api',## Connection ID created in Airflow UI
         endpoint='planetary/apod',## API endpoint
         method='GET',
         data= {'api_key': "{{conn.nasa_api.extra_dejson.api_key}}" },## API key from connection extra field
-        response_filter=lambda response: response.json(), ## Convert response to JSON
+        response_filter=lambda response: response.json(), ## Parse JSON response
+        response_check=lambda response: response.status_code == 200, ## Check for successful response
     )
 
     ## Step 3: Transform the data 
@@ -81,4 +82,11 @@ with DAG(
     ## Step 5: Verify the data load by querying the Postgres table
     
     ## Step 6: Define task dependencies  
+    create_table() >> extract_apod_data ## Ensure table is created before extraction
+    api_response = extract_apod_data.output
+    ## Transform 
+    transformed_data = transform_apod_data(api_response)
+    ## Load
+    load_data_to_postgres_task= load_data_to_postgres(transformed_data)
+
 

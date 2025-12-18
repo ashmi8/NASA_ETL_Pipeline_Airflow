1ed3a823201907900cd18e96d2f7a825db992e9b
ashmi8 <ashmijha8@gmail.com>
2025-12-18 13:22:09 +0100

    Update ETL pipeline - Extracted nasa api data

--- dags/etl.py
+++ dags/etl.py
@@ -17,8 +17,35 @@ with DAG(
     
     ## Step 1: Create the table if it doesn't exist
 
+    @task
+    def create_table():
+        ## Intialize Postgres hook
+        postgres_hook = PostgresHook(postgres_conn_id='postgres_default')
+
+        ## Create SQL table query
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS apod_data (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255),
+            explanation TEXT,
+            url TEXT,
+            date DATE
+            media_type VARCHAR(50)
+        );"""
+
+        ## Execute the create table query
+        postgres_hook.run(create_table_query)
+
     ## Step 2: Extract the NASA API Data(APOD)-Astronomy Picture of the Day[Extract pipeline]
-    
+    ## Sample API url: https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+    extract_apod_data = SimpleHttpOperator(
+        task_id='extract_apod_data',
+        http_conn_id='nasa_api',## Connection ID created in Airflow UI
+        endpoint='planetary/apod',## API endpoint
+        method='GET',
+        data= {'api_key': "{{conn.nasa_api.extra_dejson.api_key}}" },## API key from connection extra field
+        response_filter=lambda response: response.json(), ## Convert response to JSON
+    )
     ## Step 3: Transform the data 
 
     ## Step 4: Load the data into Postgres [Load pipeline]
@@ -26,4 +53,4 @@ with DAG(
     ## Step 5: Verify the data load by querying the Postgres table
     
     ## Step 6: Define task dependencies  
-    
+
